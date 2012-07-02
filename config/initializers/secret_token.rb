c21b3312c64a03d4c442afa3ac4fd4e6b95e55b1
Andrew Wu <andrewchaowu@gmail.com>
2012-07-02 14:06:03 -0500

    first save2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPrelaunchSignup::Application.config.secret_token = '1f415dd8ee540ef4fe8c45884eae25b5ae37ff882d3fe07f18aa283d13c5ba5963a2f84159052b0046d304d264c800374befc175ea2707491ab7cd4d5eaab68c'
