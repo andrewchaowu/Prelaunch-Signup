df0be1fabf0001451039024b8916b75d3c253efb
Andrew Wu <andrewchaowu@gmail.com>
2012-07-10 10:00:48 -0500

    Request Invitation

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -30,13 +30,11 @@ end
 def sign_up
   delete_user
   visit '/users/sign_up'
-  fill_in "Name", :with => @visitor[:name]
   fill_in "Email", :with => @visitor[:email]
-  fill_in "Password", :with => @visitor[:password]
-  fill_in "Password confirmation", :with => @visitor[:password_confirmation]
-  click_button "Sign up"
+  click_button "Request Invitation"
   find_user
 end
+
 
 def sign_in
   visit '/users/sign_in'
@@ -150,7 +148,7 @@ Then /^I see an unconfirmed account message$/ do
 end
 
 Then /^I see a successful sign in message$/ do
-  page.should have_content "Signed in successfully."
+  page.should have_content "Thank you for visiting the web page"
 end
 
 Then /^I should see a successful sign up message$/ do
