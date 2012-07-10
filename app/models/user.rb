9bed716455a64bf41dc52ed5af84be7289e72a60
Andrew Wu <andrewchaowu@gmail.com>
2012-07-10 16:36:35 -0500

    enable a user to set a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,5 +27,22 @@ class User < ActiveRecord::Base
     def active_for_authentication?
       confirmed? || confirmation_period_valid?
     end
+    # new function to set the password
+    def attempt_set_password(params)
+      p = {}
+      p[:password] = params[:password]
+      p[:password_confirmation] = params[:password_confirmation]
+      update_attributes(p)
+    end
+
+    # new function to determine whether a password has been set
+    def has_no_password?
+      self.encrypted_password.blank?
+    end
+
+    # new function to provide access to protected method pending_any_confirmation
+    def only_if_unconfirmed
+      pending_any_confirmation {yield}
+    end
     
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ RailsPrelaunchSignup::Application.routes.draw do
     root :to => "devise/registrations#new"
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
-  devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
   match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
   resources :users, :only => [:show, :index] do
     get 'invite', :on => :member
